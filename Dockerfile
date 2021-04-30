f230e038e17a34aeffc99e64bcbda77a1fdc9bac
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-30 16:12:49 +0200

    Add initial working version (#1)

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
-FROM alpine:3.10
+FROM openjdk:17-slim-buster
 
-# TODO: install packages required to run the tests
-# RUN apk add --no-cache jq coreutils
+RUN apt-get update && \
+    apt-get install -y curl jq && \
+    apt-get purge --auto-remove -y && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
 WORKDIR /opt/test-runner
+
+RUN curl -lO https://courses.missouristate.edu/KenVollmar/mars/MARS_4_5_Aug2014/Mars4_5.jar
+
 COPY . .
 ENTRYPOINT ["/opt/test-runner/bin/run.sh"]
--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Synopsis:
 # Run the test runner on a solution.
@@ -25,6 +25,8 @@ slug="$1"
 input_dir="${2%/}"
 output_dir="${3%/}"
 results_file="${output_dir}/results.json"
+runner_file="${input_dir}/runner.mips"
+impl_file="${input_dir}/impl.mips"
 
 # Create the output directory if it doesn't exist
 mkdir -p "${output_dir}"
@@ -33,27 +35,24 @@ echo "${slug}: testing..."
 
 # Run the tests for the provided implementation file and redirect stdout and
 # stderr to capture it
-# TODO: Replace 'RUN_TESTS_COMMAND' with the command to run the tests
-test_output=$(RUN_TESTS_COMMAND 2>&1)
+test_output=$(java -Djava.util.prefs.userRoot=/tmp -jar /opt/test-runner/Mars4_5.jar nc "${runner_file}" "${impl_file}" 2>&1)
+echo "${test_output}" | grep "all tests passed"
 
 # Write the results.json file based on the exit code of the command that was 
 # just executed that tested the implementation file
 if [ $? -eq 0 ]; then
     jq -n '{version: 1, status: "pass"}' > ${results_file}
 else
-    # OPTIONAL: Sanitize the output
-    # In some cases, the test output might be overly verbose, in which case stripping
-    # the unneeded information can be very helpful to the student
-    # sanitized_test_output=$(printf "${test_output}" | sed -n '/Test results:/,$p')
+    # Sanitize the output    
+    sanitized_test_output=$(printf "${test_output}" | sed -E -e '/^INFO/d' -e '/java\.util\.prefs\.FileSystemPreferences/d')
 
-    # OPTIONAL: Manually add colors to the output to help scanning the output for errors
-    # If the test output does not contain colors to help identify failing (or passing)
-    # tests, it can be helpful to manually add colors to the output
-    # colorized_test_output=$(echo "${test_output}" \
-    #      | GREP_COLOR='01;31' grep --color=always -E -e '^(ERROR:.*|.*failed)$|$' \
-    #      | GREP_COLOR='01;32' grep --color=always -E -e '^.*passed$|$')
+    # Manually add colors to the output to help scanning the output for errors
+    colorized_test_output=$(echo "${sanitized_test_output}" \
+         | GREP_COLOR='01;31' grep --color=always -E -e '^(failed for|Error in).*$|$')
 
-    jq -n --arg output "${test_output}" '{version: 1, status: "fail", output: $output}' > ${results_file}
+    printf "${colorized_test_output}"
+
+    jq -n --arg output "${colorized_test_output}" '{version: 1, status: "fail", output: $output}' > ${results_file}
 fi
 
 echo "${slug}: done"
